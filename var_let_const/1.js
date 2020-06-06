c63df6d9eb150fb2572ac5fe4e0ad4ac3c107301
Dgjtfgh <1137807913@qq.com>
2020-06-06 21:42:25 +0800

    js var let const 区别

---
+++ var_let_const/1.js
@@ -0,0 +1,23 @@
+// let i = 1
+for (let i = 0; i <= 3; i++) {
+  setTimeout(() => {
+    console.log(i)
+  }, 0);
+}
+// console.log(i)
+
+// let i = 1
+for (var i = 0; i <= 3; i++) {
+  setTimeout(() => {
+    console.log(i)
+  }, 0);
+}
+// console.log(i, '++++')
+
+// var i = 1
+// for (let i = 0; i <= 10; i++) {
+//   setTimeout(() => {
+//     console.log(i)
+//   }, 0);
+// }
+
